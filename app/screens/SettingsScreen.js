8fa65c3bdbcc5a08b4070b972d564465565f11d6
Ej Sobrepeña <ej.sobrepena@tuni.fi>
2024-10-22 11:39:40 +0300

    Add SettingsScreen component for managing app settings

---
+++ app/screens/SettingsScreen.js
@@ -0,0 +1,13 @@
+// app/screens/SettingsScreen.js
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const SettingsScreen = () => {
+  return (
+    <View>
+      <Text>Settings</Text>
+    </View>
+  );
+};
+
+export default SettingsScreen;
