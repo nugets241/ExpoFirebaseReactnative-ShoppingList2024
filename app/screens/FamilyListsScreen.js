c4e00ffe00bd6cf045b66411d7813a2741b2d926
Ej Sobrepeña <ej.sobrepena@tuni.fi>
2024-10-22 11:39:45 +0300

    Add FamilyListsScreen component for displaying family lists

---
+++ app/screens/FamilyListsScreen.js
@@ -0,0 +1,13 @@
+// app/screens/FamilyListsScreen.js
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const FamilyListsScreen = () => {
+  return (
+    <View>
+      <Text>Family Lists</Text>
+    </View>
+  );
+};
+
+export default FamilyListsScreen;
