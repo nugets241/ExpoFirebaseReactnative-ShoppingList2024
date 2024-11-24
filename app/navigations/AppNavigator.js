db3f44670ff4c904196dcd6b3086d3d25ce9073c
Ej Sobrepeña <ej.sobrepena@tuni.fi>
2024-11-25 00:03:56 +0200

    Update AppNavigator to use MyListsScreen and SharedListsScreen for improved navigation

--- app/navigations/AppNavigator.js
+++ app/navigations/AppNavigator.js
@@ -3,7 +3,8 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import ListsScreen from '../screens/ListsScreen';
+import MyListsScreen from '../screens/MyListsScreen';
+import SharedListsScreen from '../screens/SharedListsScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 import OnboardingScreen from '../screens/OnboardingScreen';
 import AddListScreen from '../screens/AddListScreen';
@@ -14,7 +15,8 @@ const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => (
   <Tab.Navigator>
-    <Tab.Screen name="Lists" component={ListsScreen} />
+    <Tab.Screen name="My Lists" component={MyListsScreen} />
+    <Tab.Screen name="Shared Lists" component={SharedListsScreen} />
     <Tab.Screen name="Settings" component={SettingsScreen} />
   </Tab.Navigator>
 );
@@ -24,8 +26,16 @@ const AppNavigator = () => (
     <Stack.Navigator initialRouteName="Onboarding">
       <Stack.Screen name="Onboarding" component={OnboardingScreen} />
       <Stack.Screen name="Main" component={TabNavigator} options={{ headerShown: false }} />
-      <Stack.Screen name="AddList" component={AddListScreen} />
-      <Stack.Screen name="ListDetails" component={ListDetailsScreen} />
+      <Stack.Screen
+        name="AddList"
+        component={AddListScreen}
+        options={{ title: 'Add List' }}
+      />
+      <Stack.Screen
+        name="ListDetails"
+        component={ListDetailsScreen}
+        options={({ route }) => ({ title: route.params.listName || 'List Details' })}
+      />
     </Stack.Navigator>
   </NavigationContainer>
 );
