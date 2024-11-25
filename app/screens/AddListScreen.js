5b2049ce1c62ac2e0c6bbd1d26404f989d77b998
Ej Sobrepeña <ej.sobrepena@tuni.fi>
2024-11-25 12:38:30 +0200

    Add list name uniqueness check in AddListScreen before creating a new list

--- app/screens/AddListScreen.js
+++ app/screens/AddListScreen.js
@@ -1,11 +1,11 @@
 // app/screens/AddListScreen.js
 import React, { useState, useEffect } from 'react';
 import { View, TextInput, Button, Alert } from 'react-native';
-import { createList } from '../firestoreService';
+import { createList, fetchUserLists } from '../firestoreService';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import LoadingScreen from './LoadingScreen';
 import { collection, query, where, getDocs } from 'firebase/firestore';
-import { FIRESTORE_DB } from '../../firebaseConfig'; // Make sure this path is correct
+import { FIRESTORE_DB } from '../../firebaseConfig';
 
 const AddListScreen = ({ navigation }) => {
   const [listName, setListName] = useState('');
@@ -17,14 +17,13 @@ const AddListScreen = ({ navigation }) => {
       const savedUsername = await AsyncStorage.getItem('username');
       if (savedUsername) {
         try {
-          // Get userId from users collection
           const usersCollectionRef = collection(FIRESTORE_DB, 'users');
           const userQuery = query(usersCollectionRef, where('username', '==', savedUsername));
           const userSnapshot = await getDocs(userQuery);
 
           if (!userSnapshot.empty) {
             const userDoc = userSnapshot.docs[0];
-            setUserId(userDoc.id); // Store userId
+            setUserId(userDoc.id);
           } else {
             Alert.alert('Error', 'User not found. Please log in again.');
             navigation.navigate('Onboarding');
@@ -42,11 +41,19 @@ const AddListScreen = ({ navigation }) => {
     loadUserId();
   }, [navigation]);
 
-  // app/screens/AddListScreen.js
   const handleAddList = async () => {
     if (listName.trim()) {
       setLoading(true);
       try {
+        // Check if the list name already exists
+        const userLists = await fetchUserLists(userId);
+        const listExists = userLists.some(list => list.name.toLowerCase() === listName.toLowerCase());
+        if (listExists) {
+          Alert.alert('Error', 'A list with this name already exists. Please choose a different name.');
+          setLoading(false);
+          return;
+        }
+
         await createList(listName, userId);
         Alert.alert('Success', 'List created successfully!');
         navigation.navigate('Main', { screen: 'My Lists', params: { refresh: true } }); // Pass refresh parameter
